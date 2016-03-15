8abf5d03ea4432557c64c62bcf880f7991116d83
Oscuro <Oscuro@Dark>
2016-03-14 19:49:04 -0600

    creación de proyecto
    se creo el nuevo proyecto

---
+++ CRUD_WCF/Dtos/DTO/DTOAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.DTO
+{
+   public class DTOAlumno
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string appat { get; set; }
+        public string apmat { get; set; }
+        public string curp { get; set; }
+        public DateTime? fecha_nac { get; set; }
+        public int? Id_Estado { get; set; }
+
+    }
+}
---
+++ CRUD_WCF/Dtos/Maps/MapAlumnos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dtos.DTO;
+using Persistencia.Model.Operativo;
+namespace Dtos.Maps
+{
+   public class MapAlumnos
+    {
+       /// <summary>
+       /// Mapeo Entidad Alumnos
+       /// </summary>
+       /// <param name="alumnos"></param>
+        /// <returns>DTOAlumno</returns>
+       public DTOAlumno MapAlumnosENTToDTO(alumnos alumnos)
+       {
+           DTOAlumno DTOAlumno=new DTOAlumno();
+           try
+           {
+               alumnos.Id_Estado = DTOAlumno.Id_Estado;
+               alumnos.nombre = DTOAlumno.nombre;
+               alumnos.appat = DTOAlumno.appat;
+               alumnos.apmat = DTOAlumno.apmat;
+               alumnos.curp = DTOAlumno.curp;
+               alumnos.fecha_nac = DTOAlumno.fecha_nac;
+               alumnos.Id_Estado = DTOAlumno.Id_Estado;
+
+               return DTOAlumno;
+           }
+           catch (Exception ex)
+           {
+               
+               throw ex;
+           }
+         
+         
+       }
+    }
+}
